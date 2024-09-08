a1fbc7556ae10a8ff16c13d4efa9ce15947904f3
shimiharu1012 <shimizu1012@ezweb.ne.jp>
2024-09-09 00:07:27 +0900

    [add] ジュリア集合を追加

--- Julia.py
+++ Julia.py
@@ -1,11 +1,11 @@
-
-# マンデルブロ集合をmatplotlibで描画する
+# ジュリア集合をmatplotlibで描画する
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 # x+yiは、[x,y]として定義する
+C=[0.37,0.1] # ジュリア集合における漸化式の定数C
 
 # 複素数同士の足し算
 def complex_add(z1,z2):
@@ -19,22 +19,22 @@ def complex_double(z):
 def complex_abs(z):
     return z[0]**2+z[1]**2
     
-# 複素数cがマンデルブロ条件式を満たすか否か
+# 複素数z0がジュリア条件式を満たすか否か
 # 漸化式は100項目まで、収束判定は|z|^2<4で行う
-def Mandelbrot_condition(c):
-    z=[0,0]
+def Julia_condition(z0):
+    z=z0
     for i in range(100):
-       z=complex_add(complex_double(z),c)
-       if complex_abs(z)>4:
+      z=complex_add(complex_double(z),C)
+      if complex_abs(z)>4:
         return False
     
     return True
 
 
-x_max=1
-x_min=-2.5
-y_max=1.2
-y_min=-1.2
+x_max=2
+x_min=-2
+y_max=2
+y_min=-2
 fineness=1000
 x_step=(x_max-x_min)/fineness
 y_step=(y_max-y_min)/fineness
@@ -42,15 +42,14 @@ y_step=(y_max-y_min)/fineness
 data=np.zeros((fineness,fineness))
 
 for i in range(fineness):
-    x=x_min+x_step*i
-    for j in range(fineness):
-        y=y_min+y_step*j
-        c=[x,y]
-        if Mandelbrot_condition(c):
-            data[j][i]=1
+   x=x_min+x_step*i
+   for j in range(fineness):
+      y=y_min+y_step*j
+      z0=[x,y]
+      if Julia_condition(z0):
+        data[j][i]=1
+    
+plt.imshow(data,cmap='PiYG',extent=(x_min,x_max,y_min,y_max))
 
-
-plt.imshow(data, cmap='PiYG', extent=(x_min, x_max, y_min, y_max))
-
-plt.grid(False)  # グリッド線を非表示
+plt.grid(False)
 plt.show()
